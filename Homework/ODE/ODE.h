c860dc7de72a2bb3aa4f998300e0c50552bd2a52
Tobias <tobias.washeim1@gmail.com>
2025-03-25 10:55:53 +0100

    ODE part A broken

--- Homework/ODE/ODE.h
+++ Homework/ODE/ODE.h
@@ -1,10 +1,9 @@
 // Runge-Kutta 12 class
 #include<vector>
 #include<tuple>
+#include<functional>
+#include"../Matrix/vector.h"
 
-class RK12
-{
-public:
-    std::pair<std::vector<double>,std::vector<Vector>> driver(std::function<Vector(double, Vector)> f, double a, Vector ya, double b, double h=0.125, double acc=0.01, double eps=0.01);  
-    std::vector<Vector> RK12::stepper(std::function<Vector(double, Vector)> f, double x, Vector y, double h);
-};
+std::tuple<Vector,Vector> driver(std::function<Vector(double, Vector)> f, double a, Vector ya, double b, double h=0.125, double acc=0.01, double eps=0.01);
+
+std::vector<Vector> stepper(std::function<Vector(double, Vector)> f, double x, Vector y, double h);
