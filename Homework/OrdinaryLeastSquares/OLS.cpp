7a7edd9d22d1bc85dc16d4e5955175e991b27a88
Tobias <tobias.washeim1@gmail.com>
2025-05-22 13:11:30 +0200

    HW least-squares done

--- Homework/OrdinaryLeastSquares/OLS.cpp
+++ Homework/OrdinaryLeastSquares/OLS.cpp
@@ -4,7 +4,7 @@
 #include<tuple>
 #include<iostream>
 #include"../LinearEquations/QR.h"
-#include"OLSFit.h"
+#include"OLS.h"
 
 /* 
 Ordinary Least Squares Fit using QR-decomposition 
@@ -20,22 +20,20 @@ std::tuple<colVector,matrix> lsfit(std::function<double(double)> fs[], colVector
 
     for (int i = 0; i < N; i++) {
         b[i] = y[i]/dy[i];
-        //std::cout << "test, i = " << i << "\n";
         for (int k = 0; k < size; k++) {
             A(i,k) = fs[k](x[i])/dy[i];
-            //std::cout << "i,k =" << i << ", " << k << "\n";
         }
     }
     
     // Solves least square problem using QR-composition
     std::tuple<matrix,matrix> qr_decomp = decomp(A);
-
+    
     matrix Q = std::get<0>(qr_decomp);
     matrix R = std::get<1>(qr_decomp);
 
     // We calculate the corvariance matrix
 
-    matrix coVar = inverse(transpose(A) * A);
+    matrix covariance = inverse(transpose(A) * A);
     
-    return std::make_tuple(solve(Q,R,b),coVar);
+    return std::make_tuple(solve(Q,R,b),covariance);
 }
--- Homework/OrdinaryLeastSquares/main.cpp
+++ Homework/OrdinaryLeastSquares/main.cpp
@@ -1,31 +1,15 @@
 #include<functional>
 #include<cmath>
 #include<iostream>
-#include<tuple>
 #include<vector>
-#include"../../Matrix/matrix.h"
-#include"../LinearEquations/QR.h"
-#include"OLSFit.h"
+#include<tuple>
+#include"OLS.h"
+std::tuple<colVector,matrix> radioactivityFit()  {
+    
+    std::function<double(double)> f1 = [](double x) {return 1.0;};
+    std::function<double(double)> f2 = [](double x) {return -x;};
 
 
-double func1(double x) {
-    return 1.0;
-}
-
-double func2(double x) {
-    return -x;
-}
-
-
-double fit(double x, colVector c) {
-    return c[0] - c[1] * x;
-}
-
-int main() {
-
-    // Functions
-    std::function<double(double)> f1 = func1;
-    std::function<double(double)> f2 = func2;
 
     // Data
     std::vector<double> xs = {1,2,3,4,6,9,10,13,15};
@@ -55,20 +39,68 @@ int main() {
     int size = 2;
 
     // do fitting
-    std::tuple<colVector,matrix> fit = lsfit(fs,x,y,dy,size);
+    return lsfit(fs,x,y,dy,size);
+ }
+
+int main() {
+
+    std::tuple<colVector,matrix> fit = radioactivityFit();
 
     colVector c = std::get<0>(fit);
-    matrix coVar = std::get<1>(fit);
+    matrix covariance = std::get<1>(fit);
 
-    // Print fitting result
-    std::cout << "ln(a): " << std::exp(c[0]) << "\n";
+    double logaUncertainty = covariance(0,0);
+    double lambdaUncertainty = covariance(1,1);
+    double halfLifeUncertainty = std::log(2)/c[1]/c[1] * lambdaUncertainty;
 
-    double halfLifeUncertainty = log(2)/c[1]/c[1] * coVar(1,1);
-    std::cout << "lambda: " << c[1] << " plus-minus " << halfLifeUncertainty <<"\n";
-    std::cout << "Half-life value of ThX: " << std::log(2)/c[1] << " days\n";
-    std::cout << "Correct half-life: " << 3.66 << " days\n";
-    // Fitting the function 
-    for (double i = 0; i < xs[xs.size()-1]; i += 0.1) {
-        std::cout << i << " " << std::exp(c[0]) * std::exp(-c[1]*i)  << "\n";
-    }
+    std::cout << "-----------------------------------------" << "\n";
+    std::cout << "Homework: Ordinary Least Squares - part A" << "\n";
+    std::cout << "-----------------------------------------" << "\n\n";
+
+    std::cout << "In part A we have implemented ordinary   " << "\n";
+    std::cout << "least squares fit using QR-decomposition." << "\n";
+    std::cout << "To show that it works we have fitted the " << "\n";
+    std::cout << "data from measurements of ThX activity.  " << "\n\n";
+
+    std::cout << "The plot of the measurements can be seen " << "\n";
+    std::cout << "in the folder 'plots/'. Using OLS the    " << "\n";
+    std::cout << "fitting coefficients are                 " << "\n\n";
+
+    std::cout << "ln(a) = " << std::exp(c[0]) << "," << "\n";
+    std::cout << "lambda: " << c[1] << "." << "\n";
+    
+    std::cout << "The half-life of ThX from the fitting is " << "\n\n";
+
+    std::cout << "T_(1/2) = " << std::log(2)/c[1] << " days." << "\n\n";
+    
+    std::cout << "The modern value is 3.66 days.           " << "\n\n";
+
+
+    std::cout << "-----------------------------------------" << "\n";
+    std::cout << "Homework: Ordinary Least Squares - part B" << "\n";
+    std::cout << "-----------------------------------------" << "\n\n";
+
+    std::cout << "In part B we have modified the ordinary  " << "\n";
+    std::cout << "least least squares method to calculate  " << "\n";
+    std::cout << "and return the covariance matrix. The    " << "\n";
+    std::cout << "uncertainty on the fitting coefficients  " << "\n";
+    std::cout << "are                                      " << "\n\n";
+
+    std::cout << "log(a) uncertainty = " << logaUncertainty << "\n";
+    std::cout << "Lambda uncertainty = " <<  lambdaUncertainty << "\n\n";
+
+    std::cout << "The uncertainty for the half-life is    " << "\n\n";
+    
+    std::cout << "T_(1/2) uncertainty = " << halfLifeUncertainty << "days." << "\n\n";
+
+    std::cout << "The modern half-life uncertainty for ThX " << "\n";
+    std::cout << "is 0.0014 days. Comparing the modern value" << "\n";
+    std::cout << "Comparing the modern value for the half  " << "\n";
+    std::cout << "-life from the fit and its uncertainty we" << "\n";
+    std::cout << "see that that the values does not agree  " << "\n"; 
+    std::cout << "within the estimated uncertainty.        " << "\n";
+
+    std::cout << "Source for ThX modern value and half life:" << "\n";
+    std::cout << "NIST" << " \n";
+    return 0;
 }   
